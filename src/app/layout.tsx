12a7748592d457942fccddee0761b3fda608fbe8
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-09-19 01:29:45 +0530

    fixed stuff, added font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import Navbar from "@/componenets/Navbar";
+import { Montserrat } from "next/font/google";
+import Navbar from "@/components/Navbar";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const montserratSansSerif = Montserrat({
+  variable: "--font-montserrat",
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,7 +16,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${montserratSansSerif.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <Navbar/>
