7eb491733b9ae992d970bc84b174d8db3cfd8b37
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-09-19 00:44:16 +0530

    making navbar in tailwind is hard when its my first time :pensive-wobble:

--- src/componenets/Navbar.tsx
+++ src/componenets/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav className="flex justify-center p-4 w-sm">
+        <nav className="flex justify-center items-center text-center p-5 gap-4 backdrop-blur-sm bg-[#ff1e00cc] w-3xs mx-auto">
             <Link href="/">Home</Link>
             <Link href="/replay">Replay</Link>
             <Link href="/strategy">Strategy</Link>
