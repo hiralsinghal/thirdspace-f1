68faa31e6ca18242177f9f16589c816c2dbb12c3
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-09-20 17:11:58 +0530

    designing the drivers page

--- src/app/drivers/page.tsx
+++ src/app/drivers/page.tsx
@@ -4,10 +4,10 @@ import DriverCard from "@/components/DriverCard";
 
 export default function Drivers() {
     return (
-        <div>
+        <div className="mx-auto">
             <h1 className="mt-10 mx-auto text-center font-heading text-5xl">Drivers</h1>
             
-            <div>
+            <div className="grid grid-cols-2 gap-10 mx-10">
                 {(drivers as Driver[]).map((driver) => (
                     <DriverCard key={driver.id} driver={driver} />
                 ))}
--- src/components/DriverCard.tsx
+++ src/components/DriverCard.tsx
@@ -2,10 +2,11 @@ import { Driver } from "@/types/driver";
 
 export default function DriverCard({ driver }: {driver: Driver}) {
     return (
-        <div>
-            <h2>{driver.firstName} {driver.lastName}</h2>
-            <p>{driver.number}</p>
+        <div className="relative border-4 p-5" style={{borderColor:driver.teamColor}}>
+            <span className="absolute -z-1 inset-x-0 text-5xl text-center opacity-50 text-shadow-black-300" style={{color:driver.teamColor}}>{driver.number}</span>
+            <h2 className="text-center">{driver.firstName} {driver.lastName}</h2>
             <p>Wins: {driver.wins}</p>
-        </div>
+        </div>  
+
     );
 }
