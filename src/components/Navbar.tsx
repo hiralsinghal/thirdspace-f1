d8aa580bd03898c47125a10ef4ea9524e3ea111b
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-09-21 02:04:36 +0530

    tried adding active links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,13 +1,26 @@
+"use client"
+import { usePathname } from "next/navigation";
 import Link from "next/link";
 
+const NavLinks = [
+    { name: "Home", path: "/"},
+    { name: "Drivers", path: "/drivers"},
+    { name: "Teams", path: "/teams"},
+    { name: "Replay", path: "/replay"},
+    { name: "Strategy", path: "/strategy"}
+]
+
 export default function Navbar() {
+    const pathname = usePathname();
+    const isActive = (path: string) => path === pathname;
+
     return (
-        <nav className="flex justify-center items-center text-center p-5 gap-4 backdrop-blur-sm bg-[#ff1e00cc] w-4xs mx-auto mt-5 rounded-2xl font-body">
-            <Link href="/">Home</Link>
-            <Link href="drivers">Drivers</Link>
-            <Link href="/teams">Teams</Link>
-            <Link href="/replay">Replay</Link>
-            <Link href="/strategy">Strategy</Link>
+        <nav className="flex justify-center items-center text-center p-5 gap-4 backdrop-blur-sm bg-[#ff1e00cc] w-fit mx-auto mt-5 rounded-2xl font-body">
+            {NavLinks.map((link) => {
+                return (
+                    <Link key={link.name} href={link.path} className={isActive(link.path) ? "" : ""}>{link.name}</Link>
+                )
+            })}
         </nav>
     );
 }
