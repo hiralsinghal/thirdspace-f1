9c99eb5c32535ee67f56f5b9a512e694d68f0aa9
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-09-20 17:37:04 +0530

    trying out different tailwind stuff for numbers and background

--- src/components/DriverCard.tsx
+++ src/components/DriverCard.tsx
@@ -2,9 +2,9 @@ import { Driver } from "@/types/driver";
 
 export default function DriverCard({ driver }: {driver: Driver}) {
     return (
-        <div className="relative border-4 p-5" style={{borderColor:driver.teamColor}}>
-            <span className="absolute -z-1 inset-x-0 text-5xl text-center opacity-50 text-shadow-black-300" style={{color:driver.teamColor}}>{driver.number}</span>
-            <h2 className="text-center">{driver.firstName} {driver.lastName}</h2>
+        <div className="relative border-4 p-5" style={{backgroundColor:driver.teamColor}}>
+            <span className="absolute -z-1 inset-x-0 text-5xl text-center opacity-50 text-shadow-black-300" style={{ color:`color-mix(in srgb, ${driver.teamColor} 100%, white 60%)` }}>{driver.number}</span>
+            <h2 className="text-center font-bold">{driver.firstName} {driver.lastName}</h2>
             <p>Wins: {driver.wins}</p>
         </div>  
 
