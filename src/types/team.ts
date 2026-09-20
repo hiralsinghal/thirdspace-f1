a5d9ddf3b6ab837407973dba7df460e86a7cc68b
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-09-20 20:15:47 +0530

    add export interface for teams

---
+++ src/types/team.ts
@@ -0,0 +1,10 @@
+export interface Team {
+    id: string;
+    teamName: string;
+    constructorsChampionships: number;
+    driversChampionships: number;
+    raceWins: number;
+    podiums: number;
+    polePositions: number;
+    fastestLaps: number;
+}
