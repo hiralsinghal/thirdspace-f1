cc36cd500d87e2b125535b9784f45e1b4f01aa96
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-09-20 22:33:45 +0530

    added team colors to the teams stuff

--- src/components/TeamCard.tsx
+++ src/components/TeamCard.tsx
@@ -2,14 +2,17 @@ import { Team } from "@/types/team";
 
 export default function TeamCard({ team }:{ team: Team }) {
     return (
-        <div>
-            <h2>{team.teamName}</h2>
-            <p>Constructors' Championships: {team.constructorsChampionships}</p>
-            <p>Drivers' Championships: {team.driversChampionships}</p>
-            <p>Race Wins: {team.raceWins}</p>
-            <p>Podiums: {team.podiums}</p>
-            <p>Pole Positions: {team.polePositions}</p>
-            <p>Fastest Laps: {team.fastestLaps}</p>
+        <div className="border-4 p-0 m-0 overflow-hidden" style={{backgroundColor:team.teamColor}}>
+            <h2 className="text-center p-5">{team.teamName}</h2>
+            <div className="p-5">
+                <p>Constructors' Championships: {team.constructorsChampionships}</p>
+                <p>Drivers' Championships: {team.driversChampionships}</p>
+                <p>Race Wins: {team.raceWins}</p>
+                <p>Podiums: {team.podiums}</p>
+                <p>Pole Positions: {team.polePositions}</p>
+                <p>Fastest Laps: {team.fastestLaps}</p>
+            </div>
+            
         </div>
     );
 }
--- src/types/team.ts
+++ src/types/team.ts
@@ -7,4 +7,5 @@ export interface Team {
     podiums: number;
     polePositions: number;
     fastestLaps: number;
+    teamColor: string;
 }
