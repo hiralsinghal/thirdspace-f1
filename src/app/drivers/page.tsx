b2e5ca6943061020a809491db37bccfae815b8b9
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-09-20 18:34:22 +0530

    designing the drivers card & page

--- src/app/drivers/page.tsx
+++ src/app/drivers/page.tsx
@@ -7,7 +7,7 @@ export default function Drivers() {
         <div className="mx-auto">
             <h1 className="mt-10 mx-auto text-center font-heading text-5xl">Drivers</h1>
             
-            <div className="grid grid-cols-2 gap-10 mx-10">
+            <div className="grid grid-cols-2 gap-10 mx-10 h-100">
                 {(drivers as Driver[]).map((driver) => (
                     <DriverCard key={driver.id} driver={driver} />
                 ))}
--- src/components/DriverCard.tsx
+++ src/components/DriverCard.tsx
@@ -2,10 +2,15 @@ import { Driver } from "@/types/driver";
 
 export default function DriverCard({ driver }: {driver: Driver}) {
     return (
-        <div className="relative border-4 p-5" style={{backgroundColor:driver.teamColor}}>
-            <span className="absolute -z-1 inset-x-0 text-5xl text-center opacity-50 text-shadow-black-300" style={{ color:`color-mix(in srgb, ${driver.teamColor} 100%, white 60%)` }}>{driver.number}</span>
+        <div className="relative z-0 border-4 p-5" style={{backgroundColor:driver.teamColor, borderColor:`color-mix(in srgb, ${driver.teamColor} 30%, black 30%`}}>
+            <span className="absolute -z-1 inset-0 flex items-center justify-center text-[10cqw]" style={{ color:`color-mix(in srgb, ${driver.teamColor} 100%, white 90%)` }}>{driver.number}</span> {/**/}
             <h2 className="text-center font-bold">{driver.firstName} {driver.lastName}</h2>
+            <p>{driver.nationality}</p>
+            <p>{driver.team}</p>
+            <p>{driver.championships}</p>
             <p>Wins: {driver.wins}</p>
+            <p>{driver.podiums}</p>
+            <p>{driver.careerPoints}</p>
         </div>  
 
     );
