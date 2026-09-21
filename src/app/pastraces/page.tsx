546ca76953426094265f9f5ee87241fde55b2fd4
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-09-21 06:20:24 +0530

    scrollbar, pastraces page

--- src/app/pastraces/page.tsx
+++ src/app/pastraces/page.tsx
@@ -8,30 +8,33 @@ export default function PastRaces() {
 
     return (
         <div>
-            <h1>Past Races</h1>
-            <p>Select a race. Get the Data.</p>
+            <h1 className="mt-10 mx-auto text-center font-heading text-8xl">Past Races</h1>
+            <p className="mt-0 text-center italic font-body text-xl">Data of a race!</p>
 
             <main>
-                <div>
-                    <label htmlFor="race">Race</label>
-                    <select value={selectedID} onChange={(e) => setSelectedID(e.target.value)}>
+                <div className="my-5 flex justify-center items-center mx-auto text-xl select-floatin w-fit border-4 border-gray-700">
+                    <label htmlFor="race" className="mx-1 py-2 pl-5">Race</label>
+                    <select 
+                    className="text-center select-floating-label"
+                    value={selectedID} onChange={(e) => setSelectedID(e.target.value)}>
                         {data.races.map((race) => (
                             <option key={race.id} value={race.id}>{race.title}</option>
                         ))}
                     </select>
                 </div>
 
-                <div>
-                    <h2>{race.title}</h2>
-                    <p>{race.circuit} - {race.laps} Laps</p>
-                    <p>{race.allowed}</p>
+                <div className="text-center">
+                    <h2 className="text-center font-bold text-4xl">{race.title}</h2>
+                    <p><b>Race Track:</b> {race.circuit}</p>
+                    <p><b>Laps:</b> {race.laps} Laps</p>
+                    <p><b>Compounds Allowed:</b> {race.allowed}</p>
                 </div>
 
-                <div>
-                    <h3>Result</h3>
+                <div className="text-center">
+                    <h3 className="font-bold">Result</h3>
                     <ul>
                         {race.drivers.map((driver) => (
-                            <li>P{driver.pos} {driver.code}</li>
+                            <li key={driver.code}>P{driver.pos} {driver.code}</li>
                         ))}
                     </ul>
                 </div>
