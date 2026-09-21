a1bb94fc2bbb003b20f70b7142ab403f3f5434dc
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-09-21 07:02:56 +0530

    wip home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
     <div>
       <h1 className="mt-10 mx-auto text-center font-heading text-8xl">Formula 1</h1>
+      <p className="font-body">This page is a work in progress.</p>
     </div>
   );
 }
