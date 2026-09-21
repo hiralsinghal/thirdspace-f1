34815d1dfd0acd3d6189545905996930cd95ba19
Hiral Singhal <103998329+hiralsinghal@users.noreply.github.com>
2026-09-21 07:00:16 +0530

    font-body replay

--- src/app/replay/page.tsx
+++ src/app/replay/page.tsx
@@ -4,7 +4,7 @@ export default function Replay() {
     return (
         <div>
             <h1 className="my-10 mx-auto text-center font-heading text-8xl">Replay</h1>
-            <p>This page is a work in progress</p>
+            <p className="font-body">This page is a work in progress</p>
         </div>
     );
 }
